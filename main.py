7f7cbc2107ac9e0d943d5b33b57697452d0532fb
ElielsonU <elielson7756@gmail.com>
2023-10-19 17:28:46 -0300

    feature: adicionado exemplo de criação das tabelas

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ PASSWORD = ""
 HOST = "localhost"
 PORT = "5432"
 
+# coloquem as tabela aqui dentro, dentro de string.
+# aqui está um exemplo de como criar a tabela
+tables = [
+  """
+    create table User (
+      serial int not null primary key,
+      username varchar(100) not null,
+      fullname varchar(360) not null,
+      email varchar(120),
+      password text not null,
+      photo varchar(500)
+    );
+  """,
+  
+]
+
 conn = psycopg2.connect(
     dbname=DB_NAME,
     user=USER,
